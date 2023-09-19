c416c5ce49095dfbe21eb945235118a73dbac262
Nicholas Sihite <nicholassihite05@gmail.com>
2023-09-19 20:45:50 +0700

    Menu utama

--- src/main.java
+++ src/main.java
@@ -1,5 +1,51 @@
+import java.util.*;
+
 public class main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner in = new Scanner (System.in);
+        while (true){
+            System.out.println("MENU");
+            System.out.println("1. Sistem Persamaan Linier");
+            System.out.println("2. Determinan");
+            System.out.println("3. Matriks balikan");
+            System.out.println("4. Interpolasi Polinom");
+            System.out.println("5. Interpolasi Bicubic Spline");
+            System.out.println("6. Regresi linier berganda");
+            System.out.println("7. Keluar");
+            System.out.println("Silakan pilih menu (1-7): ");
+            int opt = in.nextInt();
+            if (opt == 1){
+                System.out.println("Silakan pilih metode yang ingin digunakan: ");
+                System.out.println("1. Metode eliminasi Gauss");
+                System.out.println("2. Metode eliminasi Gauss-Jordan");
+                System.out.println("3. Matriks matriks balikan");
+                System.out.println("4. Kaidah Cramer");
+                System.out.println("Silakan pilih menu (1-4): ");
+                int opt1 = in.nextInt();
+                System.out.println("Progres belum sejauh itu.");            
+            } else if (opt == 2){
+                System.out.println("Silakan pilih metode yang ingin digunakan: ");
+                System.out.println("1. Reduksi baris");
+                System.out.println("2. Ekspansi kofaktor");
+                System.out.println("Silakan pilih menu (1-2): ");
+                int opt2 = in.nextInt();
+                System.out.println("Progres belum sejauh itu.");           
+            } else if (opt == 3){
+                System.out.println("Silakan pilih metode yang ingin digunakan: ");
+                System.out.println("1. Menggunakan matriks identitas");
+                System.out.println("2. Metode adjoin");
+                System.out.println("Silakan pilih menu (1-2): ");
+                int opt3 = in.nextInt();
+                System.out.println("Progres belum sejauh itu.");   
+            } else if (opt == 4){
+
+            } else if (opt == 5){
+
+            } else if (opt == 6){
+
+            } else if (opt == 7){
+                break;
+            }
+        }    
     }
 }
