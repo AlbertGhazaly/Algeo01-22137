d93dd0d663df642bf4325e5e77f874dc40f4a608
Albert Ghazaly <albert260306@gmail.com>
2023-09-24 21:52:57 +0700

    complete inverse method SPL

--- src/spl.java
+++ src/spl.java
@@ -1,14 +1,15 @@
 public class spl {
-    public static float[] inverse_Spl(float[][] matrix){
-        int row = matrix.length;
+    public static float[][] inverse_Spl(float[][] matrix){
         int col = matrix[0].length;
-        for (int i=0;i<[]) {
 
-        }
-        float[][] inv = invers_identity.invert(matrix[0:row][0:col-1]);
+        float[][] inv = invers_identity.invert(operator.delColAt(matrix,col-1));
+        float[][] b_matrix = operator.takeRow(matrix, col-1);
 
+        float[][] result_Equation_Matrix = operator.multiplyMatri(inv, b_matrix);
+        return result_Equation_Matrix;
 
     }
+
 
 }
 
