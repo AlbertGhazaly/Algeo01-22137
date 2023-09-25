f5de8e216b7bf69e9eeaaa0e6bbc5907e8c688ba
Albert Ghazaly <albert260306@gmail.com>
2023-09-25 13:45:50 +0700

    Add: display matrix

--- src/operator.java
+++ src/operator.java
@@ -1,44 +1,59 @@
 public class operator {
-    public static float[][] delColAt(float[][] matri,int colId){
-        int row = matri.length;
-        int col = matri[0].length;
+    public static void displayMatrix(float[][] mat){
+        int row = mat.length;
+        int col = mat[0].length;
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                if (j==col-1){
+                    System.out.println(mat[i][j]);
+
+                }else{
+                     System.out.print(mat[i][j]+" ");
+
+                }
+            }
+        }
+    }
+    public static float[][] delColAt(float[][] matrix,int colId){
+        int row = matrix.length;
+        int col = matrix[0].length;
         float[][] result = new float[row][col-1];
         for (int i=0;i<row;i++){
             for (int j=0;j<colId;j++){
-                result[i][j] = matri[i][j];
+                result[i][j] = matrix[i][j];
 
             }
         }
         for (int i=0;i<row;i++){
             for (int j=colId;j<col-1;j++){
-                result[i][j] = matri[i][j+1];
+                result[i][j] = matrix[i][j+1];
             }
         }
         return result;
     }
 
-    public static float[][] multiplyMatri(float[][] m1, float[][] m2){
-        float[][] newMatrix = new float[m1.length][m2[0].length];
+    public static float[][] multiplyMatrix(float[][] m1, float[][] m2){
+        float[][] newmatrixx = new float[m1.length][m2[0].length];
         float sum;
         for (int i=0;i<m1.length;i++){
             for (int j=0;j<m2[0].length;j++){
                 sum = 0;
                 for (int k=0;k<m1[0].length;k++)    {
-                    sum += m1[i][k]+m2[j][k];
+                    sum += m1[i][k]*m2[k][j];
                 }
-                newMatrix[i][j] = sum;
+                newmatrixx[i][j] = sum;
             }
         }
-        return newMatrix;
+        return newmatrixx;
     }
 
-    public static float[][] takeRow(float[][] matrix, int x){
-        int row = matrix.length;
-        float[][] newMatrix = new float[row][1];
+    public static float[][] takeRow(float[][] matrixx, int x){
+        int row = matrixx.length;
+        float[][] newmatrixx = new float[row][1];
         for (int i=0;i<row;i++){
-            newMatrix[i][0] = matrix[i][x];
+            newmatrixx[i][0] = matrixx[i][x];
         }
-        return newMatrix;
+        return newmatrixx;
     }
 
 }
--- src/spl.java
+++ src/spl.java
@@ -1,15 +1,25 @@
 public class spl {
+
     public static float[][] inverse_Spl(float[][] matrix){
         int col = matrix[0].length;
 
         float[][] inv = invers_identity.invert(operator.delColAt(matrix,col-1));
         float[][] b_matrix = operator.takeRow(matrix, col-1);
+        operator.displayMatrix(inv);
 
-        float[][] result_Equation_Matrix = operator.multiplyMatri(inv, b_matrix);
+        float[][] result_Equation_Matrix = operator.multiplyMatrix(inv, b_matrix);
         return result_Equation_Matrix;
 
     }
+    
+
+    // public static void main(String[] args){
+    //     float[][] m = {{1,1,1,6},
+    //     {1,2,3,14},{2,3,2,14}};
+    //     float[][] res = inverse_Spl(m);
+    //     operator.displayMatrix(res);
 
 
+    // }
 }
 
