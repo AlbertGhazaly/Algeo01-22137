87d5b7fc99c2dfd40dae6bbc52a2b8c7a4e25a2a
Albert Ghazaly <albert260306@gmail.com>
2023-09-26 22:22:44 +0700

    Add: input matrix procedure

--- src/operator.java
+++ src/operator.java
@@ -1,6 +1,18 @@
 import java.text.DecimalFormat;
+import java.util.*;
 
 public class operator {
+    public static void inputMatrix(double[][] mat){
+        Scanner in = new Scanner (System.in);
+        int row = mat.length;
+        int col = mat[0].length;
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                mat[i][j] = in.nextDouble();
+            }
+        }
+
+    }
     public static void displayMatrix(double[][] mat){ //tampilin matriks
         int row = mat.length;
         int col = mat[0].length;
